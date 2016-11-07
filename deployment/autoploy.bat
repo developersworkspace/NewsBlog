61d656b2dacc2c307b1303dd3fb07b2da3c9597f
Grantham.Smith <grantham.smith@euromonitor.com>
2016-11-07 15:51:18 +0200

    update

---
+++ deployment/autoploy.bat
@@ -0,0 +1,3 @@
+python run.py -i 198.199.83.9 -u root -p MidericK96 -r /root/builds -n NewsBlogExchangeRate D:\Git\None\NewsBlog\deployment\exchangeRateProcessor
+python run.py -i 198.199.83.9 -u root -p MidericK96 -r /root/builds -n NewsBlogRssFeed D:\Git\None\NewsBlog\deployment\rssFeedProcessor
+python run.py -i 198.199.83.9 -u root -p MidericK96 -r /root/builds -n NewsBlogWeb D:\Git\None\NewsBlog\deployment\web
